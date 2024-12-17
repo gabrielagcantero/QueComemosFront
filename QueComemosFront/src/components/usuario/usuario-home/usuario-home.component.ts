40cc059c4398d1b967e887766797aa8d241219c8
Brenda Poch <brenda.poch@despegar.com>
2024-12-17 19:54:26 -0300

    Fix menores en los tres home

--- QueComemosFront/src/components/usuario/usuario-home/usuario-home.component.ts
+++ QueComemosFront/src/components/usuario/usuario-home/usuario-home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {RouterModule} from '@angular/router';
+import { AuthService } from '../../../app/services/auth.service';
 
 @Component({
   selector: 'app-usuario-home',
@@ -8,6 +9,16 @@ import {RouterModule} from '@angular/router';
   standalone: true,
   styleUrl: '../../../styles.css'
 })
-export class UsuarioHomeComponent {
+export class UsuarioHomeComponent implements OnInit {
+  email: string | null = null;
 
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+  
+    const currentUser = this.authService.currentUserValue;
+    if (currentUser) {
+      this.email = currentUser.email;
+    }
+  }
 }
