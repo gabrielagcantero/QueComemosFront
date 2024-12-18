50232e591b793d525740936d5dd72f4c92b57003
gabrielagcantero <gabrielagcantero@gmail.com>
2024-12-18 11:17:31 -0300

    primer intento de agregar comidas

--- QueComemosFront/src/components/administrador/admin-agregar-comida/admin-agregar-comida.component.ts
+++ QueComemosFront/src/components/administrador/admin-agregar-comida/admin-agregar-comida.component.ts
@@ -1,12 +1,56 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import {RouterModule} from '@angular/router';
+import { FormsModule} from '@angular/forms';
+import {Router, RouterModule} from '@angular/router';
+import { FoodsService } from '../../../app/services/foods.service';
+import { CategoriaComida } from '../../../app/models/comida-model';
 
 @Component({
   selector: 'app-admin-agregar-comida',
-  imports: [RouterModule],
+  imports: [FormsModule, RouterModule, CommonModule],
   templateUrl: './admin-agregar-comida.component.html',
   styleUrl: '../../../styles.css'
 })
 export class AdminAgregarComidaComponent {
+    // Estructura del menú
+    comida = {
+      nombre: '',
+      precio: 0,
+      categoria: CategoriaComida.ENTRADA,
+      enCarta: true,
+      imagen: '',
+      vegetariano: false,
+      habilitado: true,
+      menus: null
+    };
+
+    constructor(private foodsService: FoodsService, private router: Router) {}
+
+    register(): void {
+          const data = {
+            nombre: this.comida.nombre,
+            precio: this.comida.precio,
+            categoria: CategoriaComida[this.comida.categoria as keyof typeof CategoriaComida],
+            enCarta: this.comida.enCarta,
+            imagen: this.comida.imagen,
+            vegetariano: this.comida.vegetariano,
+            habilitado: true,
+            menus: this.comida.menus
+          }
+
+          console.log(data);
+
+          // this.foodsService.register(data).subscribe(
+          //   (response: any) => {
+          //     alert(response);
+          //     this.router.navigate(['/admin-comidas']);
+          //   },
+          //   error => {
+          //     console.error(error);
+          //     alert('Ocurrió un error al registrar la comida.');
+          //   }
+          // );
+
+    }
 
 }
