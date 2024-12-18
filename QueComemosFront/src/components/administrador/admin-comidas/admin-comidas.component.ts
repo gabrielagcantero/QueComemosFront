ca9d78a002d525ab443bb6b7a6f16bd5118638ff
gabrielagcantero <gabrielagcantero@gmail.com>
2024-12-18 09:56:33 -0300

    Muestro comidas de la bd en la pantalla de admin

--- QueComemosFront/src/components/administrador/admin-comidas/admin-comidas.component.ts
+++ QueComemosFront/src/components/administrador/admin-comidas/admin-comidas.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import {RouterModule} from '@angular/router';
+import { FoodsService } from '../../../app/services/foods.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-comidas',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
+  providers: [FoodsService],
   templateUrl: './admin-comidas.component.html',
   styleUrl: '../../../styles.css'
 })
 export class AdminComidasComponent {
+  comidas: any[] = [];
 
+  constructor(private foodsService: FoodsService) {}
+
+  ngOnInit() {
+    this.foodsService.getItems().subscribe(data => {this.comidas = data});
+  }
 }
